a54dabf632c7a08930d57d9af8182b7cc8ef5f28
Saleh <wefightwiththem@gmail.com>
2023-07-17 19:05:09 +0330

    Add some process after login/signup

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  loggedIn: boolean = false;
   collapsed: boolean = true;
 
-  constructor(private dataStorageService: DataStorageService) {}
+  constructor(
+    private dataStorageService: DataStorageService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.authService.loggedUser.subscribe((user) => {
+      this.loggedIn = !!user;
+    });
+  }
 
   onSaveData() {
     this.dataStorageService.saveData();
